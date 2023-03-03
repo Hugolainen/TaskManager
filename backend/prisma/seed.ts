0970c8f64e82c033d897d08274f2dd90ef15d6ba
Hugo Constant <hugo.constant@valmetpartners.com>
2023-03-03 15:55:21 +0200

    Started the Dockerisation

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,6 +1,8 @@
 import { TaskType, UserType } from '@prisma/client';
 import { addDays } from 'date-fns';
-import { usersDb, tasksDb } from '../db';
+import { usersDb, tasksDb } from '../src/db';
+
+//https://www.prisma.io/docs/guides/database/seed-database
 
 const loremIpsumDesc =
   'Cupcake ipsum dolor sit amet cake jelly sesame snaps donut. Candy canes jelly macaroon pie topping. Apple pie cake liquorice bonbon chocolate cake chocolate tart. Cheesecake bonbon bonbon jelly beans pie pie oat cake marzipan croissant. Donut powder wafer cotton candy jelly-o candy chocolate bar candy. Lollipop tart fruitcake donut pastry carrot cake macaroon pudding. Halvah gummi bears marshmallow gingerbread icing topping.';
